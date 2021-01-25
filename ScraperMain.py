205d967642a7aca359165c1f0e656a372942b493
cwethanp3 <77895052+cwethanp3@users.noreply.github.com>
2021-01-25 10:48:18 -0500

    Update ScraperMain.py

--- ScraperMain.py
+++ ScraperMain.py
@@ -1,21 +1,54 @@
 import requests
-from URLHandler import URLHandler
-from URL import URL
+from URLObjects import URLBuilder, URL
+from WebsiteObjects import Website
 from bs4 import BeautifulSoup, SoupStrainer
+from urllib.parse import urljoin
+
+#Global Variables (Yikes!!)
+#TODO: save the HTML into the database, if we keep in memory we may run out
+HTMLDictList = []
+HTMLList = []
 
 def crawl(URLString, CrawlDepth):
-    #TODO: Handle Errors
-    response = requests.get(URLString)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    #TODO: actually save HTML
-    print("Pretend I'm saving.. " + soup.prettify()[1:100])
-    if (CrawlDepth != 1):
-        for link in BeautifulSoup(response.content,'html.parser', parse_only=SoupStrainer('a')):
-            if link.has_attr('href'):
-                crawl(link['href'], CrawlDepth - 1)
-                
-URLObj = URLHandler()
+    #TODO: Handle PDF's
+    if(URLString[0:4].upper() == 'HTTP'):
+        if(URLString not in HTMLList):
+            try:
+                response = requests.get(URLString)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                HTMLList.append(URLString)
+                HTMLDictList.append(dict({"URL":URLString, "HTML": soup.prettify()}))
+                print("Saved: " + URLString)
+                if (CrawlDepth > 1):
+                    for link in BeautifulSoup(response.content,'html.parser', parse_only=SoupStrainer('a')):
+                        if link.has_attr('href'):
+                            crawl(urljoin(URLString,link['href']), CrawlDepth - 1)
+            #TODO: Allow Keyboard Escape
+            except:
+                print("Unexpected Error on URL: " + URLString)
+    else:
+       print("Bad URL: " + URLString)
+
+def sendReport(website):
+    emailList = website.getEmails()
+    #TODO: make this message user changable
+    message = "Attention: The following website has changed: " + str(website.getURL())
+    for email in emailList:
+        #TODO: Send Email
+        print("Pretend I emailed: " + email)
+    
+
+#Program Mainline
+URLObj = URLBuilder()
 URLList = URLObj.getURLList()
 
 for URLItem in URLList:
-    crawl(URLItem.getURL(), URLItem.getCrawlDepth()
+    crawl(URLItem.getURL(), URLItem.getCrawlDepth())
+    #Crawl builds HTMLDictList
+for websiteData in HTMLDictList:
+    website = Website(websiteData["URL"])
+    if(website.hasChanged(websiteData["HTML"])):
+        sendReport(website)
+
+
+    
